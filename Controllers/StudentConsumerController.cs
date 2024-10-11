dbb898e11952e1e6d32d78c6699200b2d7c2cf57
souhail elghiouane <149603630+souhailelghi@users.noreply.github.com>
2024-10-11 12:54:38 +0100

    adding Studneet Service Consumer of Data UIR

---
+++ Controllers/StudentConsumerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentConsumerMicroservice.Models;
+using StudentConsumerMicroservice.Services;
+using System.Threading.Tasks;
+
+namespace StudentConsumerMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentConsumerController : ControllerBase
+    {
+        private readonly StudentServiceConsumer _studentServiceConsumer;
+
+        public StudentConsumerController(StudentServiceConsumer studentServiceConsumer)
+        {
+            _studentServiceConsumer = studentServiceConsumer;
+        }
+
+        [HttpPost("ValidateStudent")]
+        public async Task<IActionResult> ValidateStudent([FromBody] LoginRequest loginRequest)
+        {
+            // First, check if the login is valid
+            bool isValid = await _studentServiceConsumer.IsValidLoginAsync(loginRequest.Email, loginRequest.Password);
+
+            if (isValid)
+            {
+                // If valid, get the student details
+                var (codeUIR, firstName, lastName) = await _studentServiceConsumer.GetStudentDetailsAsync(loginRequest.Email, loginRequest.Password);
+                return Ok(new { CodeUIR = codeUIR, FirstName = firstName, LastName = lastName });
+            }
+
+            // If login is not valid, return Unauthorized
+            return Unauthorized(new { Message = "Invalid credentials" });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using StudentConsumerMicroservice.Services;
 using System.Text;
 using WebApiWithRoleAuthentication.Data;
 using WebApiWithRoleAuthentication.Models;
@@ -11,6 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+// Add HttpClient to the service container
+builder.Services.AddHttpClient();
+
+builder.Services.AddScoped<StudentServiceConsumer>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
